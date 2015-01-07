17dc1e15aa4aab7e8d6ed8264952e2f07341d625
Jared Smith <jared.rit@gmail.com>
2015-01-07 11:59:20 -0800

    Got a camera view running the feed into my app through a video session and preview layer.

--- camera-session/ViewController.swift
+++ camera-session/ViewController.swift
@@ -11,13 +11,48 @@ import AVFoundation
 
 class ViewController: UIViewController {
     let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    
+    func beginSession() {
+        var err : NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        
+        
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         captureSession.sessionPreset = AVCaptureSessionPresetLow
+        
         let devices = AVCaptureDevice.devices()
-        println(devices)
+        
+        // Loop through all the devices available to us
+        for device in devices {
+            // Make sure this particular device supports video
+            if device.hasMediaType(AVMediaTypeVideo) {
+                // Finally check the position of the camera
+                if device.position == AVCaptureDevicePosition.Back {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
+        }
+        
+        if captureDevice != nil {
+           beginSession()
+        }
     }
 
     override func didReceiveMemoryWarning() {
